70a80ee0da9f57bef70a73077fd3118de1b765e9
Azonfox <azonfox@gmail.com>
2023-10-17 14:13:08 +0600

    Love FIX messageBOX

--- Love2D/main.lua
+++ Love2D/main.lua
@@ -7,116 +7,97 @@
 5 - MEN на свободном поле
 6 - MEN стоит на цели
 --]]
-
+--###########################################
 -- Начальная загрузка
 function love.load()
- -- require "levels"
-  require "keyevent"
-  
-  
-  -- Два тестовых поля для отладки выигрыша
-  mARR={
-0,1,0,0,0,0,5,0,0,0,3,0,0,0,2,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
-0,0,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,
-1,1,1,0,1,0,1,1,0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0,1,1,0,1,1,1,1,1,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,0,1,0,1,1,0,0,0,0,1,
-0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,1,0,2,0,3,5,0,0,0,3,0,0,0,2,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
-0,0,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,
-1,1,1,0,1,0,1,1,0,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0,1,1,0,1,1,1,1,1,0,0,0,0,1,
-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,0,1,0,1,1,0,0,0,0,1,
-0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-}
-  
-  
-  
-  
-  --Координаты игрока
-  manx=1 
+  --Include
+  -- require "levels"  -- Все уровни
+  require "Testlevels" -- Тестовые уровни
+  require "keyevent"   -- Движения игрока
+  --Переменные
+  manx=1 --Координаты игрока XY
   many=1
   mygamelevel=1  -- начальный уровень
-  
+  -- Шрифт
   bungee_font = love.graphics.newFont("font.ttf", 30 )  
-  
   -- Картинки
-  imageGround= love.graphics.newImage("Ground.png") 
-  imageWall= love.graphics.newImage("Wall.png") 
-  imageBox= love.graphics.newImage("Box.png") 
-  imageXbox= love.graphics.newImage("Xbox.png") 
-  imageMan= love.graphics.newImage("Man.png") 
-  imageManX= love.graphics.newImage("ManX.png") 
-  imageBoxOk= love.graphics.newImage("BoxOk.png") 
-  
-  gamereset(mygamelevel) -- Выставляем уровень
-  
-end
+  imageGround=  love.graphics.newImage("Ground.png") 
+  imageWall=    love.graphics.newImage("Wall.png") 
+  imageBox=     love.graphics.newImage("Box.png") 
+  imageXbox=    love.graphics.newImage("Xbox.png") 
+  imageMan=     love.graphics.newImage("Man.png") 
+  imageManX=    love.graphics.newImage("ManX.png") 
+  imageBoxOk=   love.graphics.newImage("BoxOk.png") 
+  -- Выставляем уровень изначально
+  gamereset(mygamelevel) 
+end -- End LOAD
 
-
--- Выставляем уровень
+-- Функция установки игрового уровня по его номеру 
 function gamereset(gamelevel)
-  levelOK=0 
---Формирование текущего уровня gamepad
-if gamelevel>50 then gamelevel=50 end
-if gamelevel<1  then gamelevel=1  end
-local adr=(gamelevel-1)*19*16
-gamepad={}
- for myi=1, 16 do
-  gamepad[myi]={}
-  for mxi=1,19 do
-    adr=adr+1
-    gamepad[myi][mxi]=mARR[adr]
-    if(gamepad[myi][mxi]==5) then manx=mxi many=myi end -- игрок
-  end
- end 
-end
+  levelOK=0 -- Сброс флага выигрыша
+  -- Проверка номера уровня
+  if gamelevel>50 then gamelevel=50 end
+  if gamelevel<1  then gamelevel=1  end
+  
+  --Формирование текущего уровня gamepad из mARR
+  local adr=(gamelevel-1)*19*16
+  gamepad={}
+   for myi=1, 16 do
+    gamepad[myi]={}
+    for mxi=1,19 do
+      adr=adr+1
+      gamepad[myi][mxi]=mARR[adr]
+      if(gamepad[myi][mxi]==5) then manx=mxi many=myi end -- игрок
+    end
+   end 
+end  -- End GAMERESET
 
-
+--###########################################
 -- Рабочий процесс
 function love.update(dt)
-  -- Обработка движения
+  -- Обработка клавиатуры
   function love.keyreleased(key)
     if (key == "left")  then keyleft()  end
     if (key == "right") then keyright() end
     if (key == "up")    then keyup()    end  
     if (key == "down")  then keydown()  end
-    if (key == " " or key == "space")  then  gamereset(mygamelevel) end   
+    if (key == " " or key == "space")  then  
+                 gamereset(mygamelevel) end   
     if (key == "return" and mygamelevel<50) then  
       mygamelevel=mygamelevel+1
       gamereset(mygamelevel) 
     end    
   end
    
-  -- Проверка выигрыша - есть ли свободные ящики?
+  -- Проверка выигрыша - есть ли вообще свободные ящики?
   levelOK=1
   for myi=1, 16 do
    for mxi=1,19 do
-    if(gamepad[myi][mxi]==3) then levelOK=0 bbb=0 end -- если еще есть, то сбросим флаг
+    -- если еще есть, то сбросим флаг
+    if(gamepad[myi][mxi]==3) then 
+        levelOK=0 
+        flagOk=0 
+    end 
    end
   end 
-    -- Если выигрыш - флаг установлен, то
-    if(levelOK==1) then bbb=bbb+1 end
-  
- if(bbb==3) then 
-    local button = {"Далее2","Сначала1"}
+  -- Если выигрыш - флаг установлен, то
+  if(levelOK==1) then flagOk=flagOk+1 end
+  if(flagOk==20) then -- Время красного поля перед Message. Где *dt???
+    local button = {"Выход3","Далее2","Сначала1"}
     love.graphics.setColor(255,0,0,255)
-    aaa=love.window.showMessageBox("Ура!","Вы выиграли",button)
+    keyMess=love.window.showMessageBox("Поздравляем!","Вы выиграли",button)
     levelOK=0 
-    if aaa==1 then  mygamelevel=mygamelevel+1 end
-    if aaa==2 then  mygamelevel=mygamelevel   end     
-      if (mygamelevel<50) then  
+    if keyMess==1 then  love.event.quit()         end -- Выход
+    if keyMess==2 then  mygamelevel=mygamelevel+1 end -- Далее
+    if keyMess==3 then  mygamelevel=mygamelevel   end -- Сначала
+    if (mygamelevel<50) then  
         gamereset(mygamelevel) 
       end
-    
   end
-end
+end -- End UPDATE
   
- -- Прорисовка
+--########################################### 
+ -- Прорисовка игрового поля
 function love.draw()
   for myi=1, 16 do
     for mxi=1,19 do
@@ -130,16 +111,16 @@ function love.draw()
     end
   end 
   
-      -- Печать номера уровня
+  -- Печать номера уровня голубыми цифрами
   love.graphics.setFont( bungee_font )
-  love.graphics.setColor(0,0,255,255) -- голубой
+  love.graphics.setColor(0,0,255,255) 
   love.graphics.print(mygamelevel, 3, 3)
-  -- Восстановливаем фон
-  love.graphics.setColor(255,255,255,255)
-    -- Если выигрыш то все красное
-   if(bbb~=0) then 
-    --     love.graphics.print(aaa, 53, 3) 
+
+  -- Если выигрыш то всё красное,
+  -- иначе восстанавливаем фон
+  if(flagOk~=0) then 
     love.graphics.setColor(255,0,0,255)
-   end 
-  
-end
+  else
+    love.graphics.setColor(255,255,255,255)
+  end 
+end  -- End DRIWE
---
+++ mYLUA/user.lua
@@ -0,0 +1,11 @@
+--[[--
+  Use this file to specify **System** preferences.
+  Review [examples](+C:\ZeroBraneStudio\cfg\user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
+--]]--
+
+
+-- ќтключить перенос слов  в длинных строках
+editor.usewrap = false
+
+-- ¬ключить русификацию IDE (requires a file in cfg/i18n folder)
+language = "ru"
